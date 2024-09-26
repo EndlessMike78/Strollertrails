0082bbf8dd27a65d75faaef7b5682847865e99ef
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2024-09-26 12:41:42 -0700

    test auto pull

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php
-
-$password = $_GET["password"];
-
-if ($password != "supersecret") {
-    echo "bad password";
-    die();
-}
-
-$fileContents = file_get_contents("./hiddenfile.wtf");
-echo $fileContents;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    auto pull works woot wooooooooooooooooot
+</body>
+</html>
